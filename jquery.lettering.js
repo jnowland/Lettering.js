4274469b33657168c39b42fce8ea636b84be2d74
Ryan Seddon <seddon.ryan@gmail.com>
2010-09-15 16:44:57 -0700

    Removed the need for jQuery

--- jquery.lettering.js
+++ jquery.lettering.js
@@ -1,67 +1,36 @@
-/*!	
-* Lettering.JS 0.6 
-*
-* Copyright 2010, Dave Rupert http://daverupert.com
-* Released under the WTFPL license 
-* http://sam.zoy.org/wtfpl/
-*
-* Thanks to Paul Irish - http://paulirish.com - for the feedback.
-*
-* Date: Mon Sep 13 11:54:00 2010 -0600
-*/
-(function($){
-	var methods = {
-		init : function() {
-
-			return this.each(function() {
-				return injector($(this), '', 'char', '');
-			});
-
-		},
-
-		words : function() {
-
-			return this.each(function() {
-				return injector($(this), ' ', 'word', ' ');
-			});
-
-		},
+/* Released under the WTFPL license
+ * http://sam.zoy.org/wtfpl/
+ */
+var lettering = lettering || {};
 		
-		lines : function() {
-
-			return this.each(function() {
-				var t = $(this), r = "eefec303079ad17405c889e092e105b0";
-				// Because it's hard to split a <br/> tag consistently across browsers,
-				// (*ahem* IE *ahem*), we replaces all <br/> instances with an md5 hash 
-				// (of the word "split").  If you're trying to use this plugin on that 
-				// md5 hash string, it will fail because you're being ridiculous.
-				t.children("br").replaceWith(r);
-				return injector(t, r, 'line', '');
-			});
-
-		}
+(function(){
+	
+	var opts = {
+		"chars": ["char",""],
+		"words": ["word"," "],
+		"lines": ["line","<br />"]
 	};
-
-	function injector(t, splitter, klass, after) {
-		var a = t.text().split(splitter), inject = '';
-		if(a.length > 0) {
-			$(a).each(function(i, item) {
-				inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
-			});	
-			t.empty();
-			t.append(inject);
+	
+	lettering.splitter = function (e,t) { // Element, type
+		var type = opts[t || "chars"], splitBy = type[1], d = '', c = 0, arr, a;
+			
+		arr = e.childNodes;
+		for(var i = 0,len = arr.length; i<len; i++) {
+			if(arr[i].nodeType === 3 && type[0] === "line") {
+				c++;
+				d += ["<span class='",type[0]+c,"'>",arr[i].nodeValue,"</span>",splitBy].join("");
+			} else if(arr[i].nodeType === 3 && type[0] !== "line") {
+				a = arr[i].data.split(splitBy);
+			
+				for(var k = 0,len2 = a.length; k<len2; k++) {
+					c++;
+					d += ["<span class='",type[0]+c,"'>",a[k],"</span>",splitBy].join("");
+				}
+			}
 		}
-	}
-
-	$.fn.lettering = function( method ) {
-		// Method calling logic
-		if ( methods[method] ) {
-			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( method == 'letters' || ! method ) {
-			return methods.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
-		}
+		c = 0;
+		
+		e.innerHTML = d;
 	};
-
-})(jQuery);
+	
+})();
