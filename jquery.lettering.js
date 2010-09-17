3cdfc97c94e60fa13b2c84377ae7b8810b5f1501
Ryan Seddon <seddon.ryan@gmail.com>
2010-09-16 22:13:54 -0700

    Add odd/even functionality

--- jquery.lettering.js
+++ jquery.lettering.js
@@ -12,19 +12,21 @@ var lettering = lettering || {};
 	};
 	
 	lettering.splitter = function (e,t) { // Element, type
-		var type = opts[t || "chars"], splitBy = type[1], d = '', c = 0, arr, a;
+		var type = opts[t || "chars"], splitBy = type[1], d = '', c = 0, arr, a, m;
 			
 		arr = e.childNodes;
 		for(var i = 0,len = arr.length; i<len; i++) {
 			if(arr[i].nodeType === 3 && type[0] === "line") {
 				c++;
-				d += ["<span class='",type[0]+c,"'>",arr[i].nodeValue,"</span>",splitBy].join("");
+				m = c%2;
+				d += ["<span class='",type[0]+c,((m!==0)?" even":" odd"),"'>",arr[i].nodeValue,"</span>",splitBy].join("");
 			} else if(arr[i].nodeType === 3 && type[0] !== "line") {
 				a = arr[i].data.split(splitBy);
 			
 				for(var k = 0,len2 = a.length; k<len2; k++) {
 					c++;
-					d += ["<span class='",type[0]+c,"'>",a[k],"</span>",splitBy].join("");
+					m = c%2;
+					d += ["<span class='",type[0]+c,((m!==0)?" even":" odd"),"'>",a[k],"</span>",splitBy].join("");
 				}
 			}
 		}
